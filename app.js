d2cd9edf57b7c083fd960d42e5b50e45ca2734de
Prins Kamariya <kamriyaprince@gmail.com>
2022-06-21 22:00:29 +0530

    Added procfile, gitignore, update ports

--- app.js
+++ app.js
@@ -339,6 +339,11 @@ app.post('/send',function(req,res){
 
 });
 
-app.listen(process.env.PORT || 3000, function() {
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 3000;
+}
+
+app.listen(port, function() {
     console.log("Server is started on port 3000");
 })
